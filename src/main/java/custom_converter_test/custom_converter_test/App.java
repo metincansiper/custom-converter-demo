48851fcae1f14449c258d440bc4880e2748571d6
metincansiper <metincansiper@gmail.com>
2021-09-09 12:15:12 -0700

    Added comments and fixed some indentation mistakes

--- src/main/java/custom_converter_test/custom_converter_test/App.java
+++ src/main/java/custom_converter_test/custom_converter_test/App.java
@@ -10,34 +10,45 @@ import com.google.gson.stream.JsonReader;
 
 import factoid.converter.FactoidToBiopax;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws FileNotFoundException
     {
-    		Gson gson = new Gson();
+	// Google json library instance
+    	Gson gson = new Gson();
+	// create a reader for factoid input json data
         JsonReader reader = new JsonReader(new FileReader(App.class
           .getResource("/data.json").getFile()));
+	// create the json object from the file reader
+	// it will be the input for factoid json to biopax conversion
         JsonObject template = gson.fromJson(reader, JsonObject.class);
+	// we need to attach the absolute path of the groovy script to the data that we read from
+	// data.json file for each of the interactions
+	// each groovy script tells what must be done for the conversion
         String scriptRelPath = "src/main/resources/GroovyIntn.groovy";
+	// absolute path of the project
         String projectPath = System.getProperty("user.dir");
-		String scriptPath = projectPath + "/" + scriptRelPath;
+	// finilize the script path by adding the relative path of script to the absolute path of the project
+	String scriptPath = projectPath + "/" + scriptRelPath;
+	// iterate through all of the custom interactions and add the scriptPath property to them
         for ( JsonElement el : template.get("interactions").getAsJsonArray() ) {
         		JsonObject obj = el.getAsJsonObject();
         		if ( obj.get("type").getAsString().equals("Custom Interaction") ) {
         			obj.addProperty("scriptPath", scriptPath);
         		}
         }
+	// create a factoid to biopax converter instance
         FactoidToBiopax converter = new FactoidToBiopax();
+	// add the conversion template (the data in JsonObject format) to the factoid model
+	// so that it will be used during the conversion
         converter.addToModel(template);
         
+	// make the validation on the biopax modal in memory
         String validationRes = converter.validate();
         System.out.println("validation result:");
         System.out.println(validationRes);
         
+	// export biopax model to a string
         String res = converter.convertToBiopax();
         System.out.println("conversion result:");
         System.out.println(res);
